a944d4a28b50501a06fbdbea2a5030dafaa3dada
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-03-21 13:11:40 +0300

    Update Mortgage_Calculator.java

--- Mortgage_Calculator.java
+++ Mortgage_Calculator.java
@@ -4,24 +4,26 @@ import java.util.Scanner;
 
 public class Mortgage_Calculator{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in); //initializing the "input method"
+        try (Scanner scanner = new Scanner(System.in)) { //initializing the "input method"
         
-        System.out.print("Principal: ");
-        float principal = scanner.nextInt();
+            System.out.print("Principal: ");
+            float principal = scanner.nextInt();
         
-        System.out.print("annual rate: ");
-        float annual_rate = scanner.nextFloat();
+            System.out.print("annual rate: ");
+            float annual_rate = scanner.nextFloat();
         
-        System.out.print("period(years): ");
-        int period = scanner.nextInt();
+            System.out.print("period(years): ");
+            int period = scanner.nextInt();
         
-        double rate = ((annual_rate)/1200); // dividing by 100 since it is a percent and dividing it by 12 number of months in a year
-        double numerator = rate *(Math.pow((1 + annual_rate), (period * 12))); // part of the formula
-        double denominator = Math.pow((1 + annual_rate), (period * 12)) - 1;
-        double result = principal * (numerator/denominator); 
+            double rate = ((annual_rate)/1200); // dividing by 100 since it is a percent and dividing it by 12 number of months in a year
+            double numerator = rate *(Math.pow((1 + annual_rate), (period * 12))); // part of the formula
+            double denominator = Math.pow((1 + annual_rate), (period * 12)) - 1;
+            double result = principal * (numerator/denominator); 
         
-        String mortgage = NumberFormat.getCurrencyInstance().format(result); // changing it in to a currency format under number format class
-        System.out.println("mortgage: " + mortgage);
+            String mortgage = NumberFormat.getCurrencyInstance().format(result); // changing it in to a currency format under number format class
+            System.out.println("mortgage: " + mortgage);
+        }
+            
     
     }
 
