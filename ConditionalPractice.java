0e699698ecdc3f825a9a600c99efef3739c8222d
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-03-22 10:34:19 +0300

    Update ConditionalPractice.java

--- ConditionalPractice.java
+++ ConditionalPractice.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
 
 public class ConditionalPractice {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) { //creating an input method
             System.out.print("Number: ");
             int number = scanner.nextInt();
-
-            if (number%5 == 0 && number%3 ==0)
-                System.out.println("FizzBuzz");
-            
-            else if (number%3 == 0 ) 
+            if (number%5 == 0 && number%3 ==0) //checking if its divisible by both 3 and 5
+                System.out.println("FizzBuzz");  
+            else if (number%3 == 0 ) //checking if its only divisible by 3
                 System.out.println("Buzz");
-            else if (number%5 == 0)
+            else if (number%5 == 0) //checking if its only divisible by 5
                 System.out.println("Fizz");
-            
             else 
-                System.out.println(number);
+                System.out.println(number); //displaying the number itself
         }
     }
 }
