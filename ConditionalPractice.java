fdfc7e3f5766b7f42ca3d0957fa82824b4619d46
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-03-22 10:32:10 +0300

    Add files via upload

---
+++ ConditionalPractice.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ConditionalPractice {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Number: ");
+            int number = scanner.nextInt();
+
+            if (number%5 == 0 && number%3 ==0)
+                System.out.println("FizzBuzz");
+            
+            else if (number%3 == 0 ) 
+                System.out.println("Buzz");
+            else if (number%5 == 0)
+                System.out.println("Fizz");
+            
+            else 
+                System.out.println(number);
+        }
+
+
+    }
+}
+    
+
