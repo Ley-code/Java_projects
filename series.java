468aae1db248709cfb1b8aa6f384975ee239e77f
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-04-06 11:13:44 +0300

    Update series.java

--- series.java
+++ series.java
@@ -1,18 +1,15 @@
 public class series {
     //Question 5.23
     public static void main(String[] args) {
-        
-        int n = 5000;
         double sum = 0.0;
         double sum2 = 0.0;
-        for(int i = 1; i<=2;i++){
-            sum = sum + (double)(1/i);
+        for(double i = 1.0; i<=5000.0;i++){
+            sum = sum + (double)(1.0/i);
         }
         System.out.println(sum);
-        for (int i = 5000; i>=1;i--){
-            sum2 = sum2 + (double)(1/i);
+        for (double i = 5000.0; i>=1.0;i--){
+            sum2 = sum2 + (double)(1.0/i);
         }
         System.out.println(sum2);
-
     }
 }
