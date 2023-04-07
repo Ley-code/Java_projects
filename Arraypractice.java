883d71706705adbefb18e9c6751211be1a4d534e
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-04-07 19:58:27 +0300

    Update Arraypractice.java

--- Arraypractice.java
+++ Arraypractice.java
@@ -12,7 +12,7 @@ public class Arraypractice {
             System.out.println();
         }
         
-        //Question = Write a java program to sort a numeric array and a string array.
+        //Question 1 = Write a java program to sort a numeric array and a string array.
         int[] numbers = {2,9,4,8,1,5,6,3,2};
         String[] words = {"Chala","Abebe", "kebede", "Calcium","Bitches"};
         
@@ -23,7 +23,7 @@ public class Arraypractice {
         System.out.printf("sorted word array: %s", Arrays.toString(words));
         System.out.println();
 
-        //Question = Write a java program to sum values of an array.
+        //Question 2 = Write a java program to sum values of an array.
         double sum = 0.0;
         for (int i = 0; i<numbers.length;i++){
             sum += numbers[i];
@@ -31,7 +31,7 @@ public class Arraypractice {
         System.out.printf("Sum of numeric array: %.2f", sum);
         System.out.println();
 
-        //Question = Write a java program to print the following grid.
+        //Question 3 = Write a java program to print the following grid.
         int[][] list = new int[10][10];
         for (int i = 0; i< list.length;i++){
             for (int j = 0; j < list[i].length;j++){
@@ -41,28 +41,28 @@ public class Arraypractice {
         }
         System.out.println();
 
-        //Question = write a java program to calculate the average value of array elements.
+        //Question 4 = write a java program to calculate the average value of array elements.
         System.out.printf("average of numeric array: %.2f",(sum / numbers.length));
         System.out.println();
 
-        //Question = write a java program to find the index of an array element.
+        //Question 5 = write a java program to find the index of an array element.
         Scanner kb = new Scanner(System.in);
         System.out.print("Want to find the indexof: ");
         int number = kb.nextInt();
-        boolean exists = true;
+        boolean exists = false;
         for (int i = 0; i<numbers.length;i++){
             if (numbers[i] == number){
                 System.out.printf("the index of %d is: %d ",number,i);
+                exists = true;
                 break;
-            } else {
-                exists = false;
-            }
+            } 
         }
         if(exists == false){
             System.out.println("The number is not on the list");
         }
+        System.out.println();
 
-        //Question = write a java program to test if an array contains a specific value.
+        //Question 6 = write a java program to test if an array contains a specific value.
         boolean isthere = false;
         System.out.print("Specific value: ");
         number = kb.nextInt();
@@ -73,9 +73,13 @@ public class Arraypractice {
         }
         System.out.println(isthere);
 
-            
-        
-        
-
+        //Question 7 =  Write a Java program to remove a specific element from an array.
+        System.out.print("index of the number to be removed: ");
+        number = kb.nextInt();
+        for (int i = number; i < numbers.length -1 ; i++){
+            numbers[i] = numbers[i+1];
+        }
+        System.out.println(Arrays.toString(numbers));
+        kb.close();
     }
 }
