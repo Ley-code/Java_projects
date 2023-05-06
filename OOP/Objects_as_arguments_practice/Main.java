e61e276f57593da54cb158526ffd495e152d442c
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-05-06 15:50:50 +0300

    Update Main.java

--- OOP/Objects_as_arguments_practice/Main.java
+++ OOP/Objects_as_arguments_practice/Main.java
@@ -2,11 +2,12 @@ package OOP;
 
 public class Main {
     public static void main(String[] args) {
-        Plane plane1 = new Plane("boeing 777","Addis Ababa" , "Mexico");
+        Plane plane1 = new Plane("boeing 777","Addis Ababa" , "Mexico");  //creating an instance of the plane class
+        Airport bole_international_airport = new Airport();               //creating an instance of the airport class
         
-        plane1.fly();
-        Airport bole_international_airport = new Airport();
-        bole_international_airport.dock(plane1);
-        bole_international_airport.maintainance(plane1);
-    }
+        plane1.fly(); 
+        
+        bole_international_airport.dock(plane1); // this will take plane1 as an argument and passes it to the dock function(method) and whatever is in the dock method is called
+        bole_international_airport.maintainance(plane1); //same as the above
+    } 
 }
