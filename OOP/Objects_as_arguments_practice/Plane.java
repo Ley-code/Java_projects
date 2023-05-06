72b79aa1d039b26335584b7ae7088132d12e12d7
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-05-06 15:46:36 +0300

    Update Plane.java

--- OOP/Objects_as_arguments_practice/Plane.java
+++ OOP/Objects_as_arguments_practice/Plane.java
@@ -3,12 +3,12 @@ package OOP;
 public class Plane {
     String name,currcurrentposition,finaldestination;
     
-    Plane(String name, String currentposition, String finaldestination){
+    Plane(String name, String currentposition, String finaldestination){ //creating the constructer
         this.name = name;
         this.currcurrentposition =currentposition;
         this.finaldestination = finaldestination;
     }
-    void fly(){
+    void fly(){  //gives the plane a fly method
         System.out.println(this.name + " is flying from " + this.currcurrentposition + " to " + this.finaldestination);
     }
 }
