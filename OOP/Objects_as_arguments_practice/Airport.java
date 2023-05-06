1f636e1f831598a73c1fe4a13b9bed29e8228e8e
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-05-06 15:47:58 +0300

    Update Airport.java

--- OOP/Objects_as_arguments_practice/Airport.java
+++ OOP/Objects_as_arguments_practice/Airport.java
@@ -1,10 +1,10 @@
 package OOP;
 
 public class Airport {
-    void dock(Plane whatever){
+    void dock(Plane whatever){  //this takes the plane object as an argument to the method dock ... the name can be whatever :)
         System.out.println(whatever.name + " is parked");
     }
-    void maintainance(Plane dontknow){
+    void maintainance(Plane dontknow){ // same as the above
         System.out.println(dontknow.name + " is being maintained");
     }
 }
